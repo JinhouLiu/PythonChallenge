8e02471ae6f0597efc4113197b7f2b93cdc06e7a
David <www.975764577@qq.com>
2014-11-13 17:44:59 +0800

    python2系列和python3系列写法的不同之处
    author  liujinhou

--- PythonChallengeOne.py
+++ PythonChallengeOne.py
@@ -1,5 +1,11 @@
+
+//python version   3.4.2 
 import  math
 print(int(math.pow(2,38)))
 raw_input()
 
 //刚学python,没有掌握自动输入，输出的方法，只能手动的将结果输入到网址中，这点也许可以改进
+//python  version 2.7.8
+from math import pow
+print(int(pow(2,38)))
+raw_input()
